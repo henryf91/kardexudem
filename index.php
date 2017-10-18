889005115fcdd775b998e6f713f2611ca4b3671e
becario DITSI <BECARIODITSI@campus.udem.edu.mx>
2017-10-18 15:33:55 -0500

    comentario

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html style="height: 100%">
+<head>
+	<title>Kardex</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/jquery-3.2.1.min.js"></script>
+</head>
+
+<body style="height: 100%">
+
+	<div class="main-container">
+		<div id="main-text" style="position: relative;">
+			<div class="point">
+			</div>
+			<div id="fixed-info">
+				
+				<img class="udem-logo" src="img/udem-logo.png">
+				<p id="kardex-title">
+					Kárdex
+				</p>
+				<p id="kardex-info">
+					El Modelo Formativo de la Universidad de Monterrey, busca contribuir directa e intencionadamente en el perfil del egresado
+				</p>
+
+				<div id="name-profile-pic-container">
+					<div style="width: 50%">
+						<div id="name-student">
+						Alessandra Francesca
+						</div>
+					</div>
+					<div style="width: 50%">
+						<div id="last-name-student">
+						Fernández de Katznelson
+						</div>
+					</div>
+					<div id="profile-pic" style="background-image: url('img/profile-pic.jpg');">
+					</div>
+
+				</div>	
+			</div>		
+			<div id="meritos">
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-left: 50%;">
+						<div style="display: inline-flex; flex-direction: row">
+							<div class="meritos-title" style="background-color: #EA4863;">
+								Excelente Profesionista
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon1.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<div class="merito-text" style="font-size: 12px; width: 90%; overflow: hidden;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-right: auto;">
+						<div style="display: inline-flex; flex-direction: row-reverse;">
+							<div class="meritos-title" style="background-color: #6B4998;">
+								Íntegro como persona
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon2.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%; margin-left: auto;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-left: auto;">
+						<div style="display: inline-flex; flex-direction: row">
+							<div class="meritos-title" style="background-color: #25B4B1;">
+								Proactivo al servicio de la comunidad
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon3.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-right: auto;">
+						<div style="display: inline-flex; flex-direction: row-reverse">
+							<div class="meritos-title" style="background-color: #76B83C;">
+								Posee una formación multicultural
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon4.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%; margin-left: auto;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-left: auto;">
+						<div style="display: inline-flex; flex-direction: row">
+							<div class="meritos-title" style="background-color: #FFD32A;">
+								Indaga y soluciona problemas con pensamiento crítico
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon5.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-right: auto;">
+						<div style="display: inline-flex; flex-direction: row-reverse">
+							<div class="meritos-title" style="background-color: #0097C6;">
+								Mantiene relaciones interpersonales constructivas
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon6.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%; margin-left: auto;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="meritos-container">
+					<div class="meritos-main-div" style="margin-left: auto;">
+						<div style="display: inline-flex; flex-direction: row">
+							<div class="meritos-title" style="background-color: #FF983D;">
+								Aprende a lo largo de la vida
+							</div>
+							<div class="meritos-icon-div">
+								<img class="merito-icon" src="img/icon7.png">
+							</div>
+						</div>
+						<div style="padding: 8px 15px">
+							<p style="font-size: 12px; width: 90%"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="udem-description">
+			<div style="display: inline-flex; flex-direction: row; margin-top: 25px; margin-bottom: 25px">
+				<div id="description-container" style="width: 13%; position: relative;">
+					<img src="img/icon-text.png" style="height: 100px; left: 0; right: 0; bottom: 0; top: 0; position: absolute; margin: auto">
+				</div>
+				<div style="width: 87%; padding: 5px 15px; border-left: 2px solid #000">
+					<p class="text-description" style="font-size: 12px">La Universidad de Monterrey es una institución de origen humanista e inspiración católica que promueve la formación integral del alumno para que, a lo largo de su vida sea capaz de alcanzar la plenitud y la trascendencia.</p>
+
+					<p class="text-description" style="font-size: 12px">Para lograr lo anterior, la UDEM cuenta con un modelo educativo personalizado que atiende en el alumno tanto el aspecto académico como el formativo, logrado con ello no solo forjar excelentes profesionistas sino también extraordinarias personas.</p>
+
+					<p class="text-description" style="font-size: 12px">El modelo busca desarrollar un conjunto de competencias específicas que están estrechamente ligadas a los temas que hoy en día son especialmente relevantes: la inteligencia emocional, las relaciones interpersonales, la conciliación y colaboración, la gestión creativa del cambio, la ciudadanía responsable y la interdisciplinariedad.</p>
+
+					<p class="text-description" style="font-size: 12px">Independientemente del programa académico elegido, el involucramiento de los alumnos en estos temas y el desarrollo
+					de dichas competencias se vuelve indispensable para la formación de egresados que, más allá de tener un excelente
+					dominio de su profesión, cuenten con las herramientas para alcanzar el éxito en su vida personal y profesional, así
+					como para contribuir a la construcción de una mejor sociedad.</p>
+				</div>
+			</div>
+		</div>
+		<div id="footer" style="padding: 15px; text-align: right;">
+			<span style="cursor: pointer; text-decoration: underline; color: blue; font-size: 15px">Imprimir PDF</span>
+		</div>
+
+
+	</div>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		   $(".point").delay(120).animate({opacity: 1,height: "100%"}, 1800);
+		   $("#name-profile-pic-container").animate({opacity: 1}, 600);
+		   $(".meritos-main-div").delay(600).animate({opacity: 1}, 1900);
+		});
+	</script>
+	
+</body>
+</html>
